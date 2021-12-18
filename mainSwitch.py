8e4ade436926c4728aaf85b304a6fa88a0cb3bea
p-web-git <pmjg@pm.me>
2021-12-18 15:44:26 +0000

    add Sentry

--- mainSwitch.py
+++ mainSwitch.py
@@ -1,12 +1,16 @@
 from switch_states import SwithStateMachine
+from mqttClient import mqttClient
+from dotenv import find_dotenv, load_dotenv
+import os
 import switch_states
-
 import logging
 import time
-import mqttClient
 import json
 import signal
 import sys
+import sentry_sdk
+
+load_dotenv(find_dotenv())
 
 def processNewEventOnSM(client, userdata, msg):
 	global device
@@ -29,22 +33,29 @@ def sendOffCommand():
 	global mqttWled
 	mqttWled.send(msg='OFF')
 
+sentry_sdk.init(
+    os.getenv('SENTRY_SWITCH'),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(format='%(levelname)-8s %(message)s', level=logging.DEBUG)
 logging.info('Start')
 
 termninateFlag = False
 
 device = SwithStateMachine()
 
-mqttWled = mqttClient.mqttClient(topic='wled/room', send_status = False)
+mqttWled = mqttClient(topic='wled/room', send_status = False)
 switch_states.sendIncrementCommand = sendIncrementCommand
 switch_states.sendOnCommand = sendOnCommand
 switch_states.sendOffCommand = sendOffCommand
 switch_states.sendApiCommand = sendApiCommand
 
 
-mqttSwitch =  mqttClient.mqttClient(send_status = False)
+mqttSwitch =  mqttClient(send_status = False)
 mqttSwitch.subscribe('zigbee/wireless_switch/#')
 mqttSwitch.recvHandler(processNewEventOnSM)
 
